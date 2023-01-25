eb7c500fc40e8a1b65cccfe81c6e43319e32951d
shotastage <hornet.live.mf@gmail.com>
2023-01-25 19:57:49 +0900

    add wasm config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.experiments = {
+      asyncWebAssembly: true,
+    };
+    config.output.webassemblyModuleFilename = (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
+    return config;
+  },
   experimental: {
     appDir: true,
   },

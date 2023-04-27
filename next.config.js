c5a40f34bec9a0aaa89cac4fa7dac9c375188280
shotastage <hornet.live.mf@gmail.com>
2023-04-27 17:54:39 +0900

    update template

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true
-    };
-    config.output.webassemblyModuleFilename = (isServer ? '../' : '') + 'static/wasm/webassembly.wasm';
-    return config;
-  },
-  experimental: {
-    appDir: true,
-  },
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig

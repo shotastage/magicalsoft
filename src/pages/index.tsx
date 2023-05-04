57e269e5d26d31427440f2620d212a201efa061f
shotastage <hornet.live.mf@gmail.com>
2023-05-04 15:36:24 +0900

    add new logo file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-
+import styles from './index.module.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -116,35 +116,15 @@ export default function Home() {
             }}
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our next round of funding.{' '}
-              <a href="#" className="font-semibold text-indigo-600">
-                <span className="absolute inset-0" aria-hidden="true" />
-                Read more <span aria-hidden="true">&rarr;</span>
-              </a>
+        <div className="mx-auto max-w-7xl py-32 sm:py-48 lg:py-56">
+          <div className="flex flex-row">
+            <div className="basis-1/2">
+              <object data="/mglogo.svg" type="image/svg+xml"/>
             </div>
-          </div>
-          <div className="text-center">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Magicalsoft
-            </h1>
+            <div className="basis-1/2">
+              <div className={styles.headCard}>SSSS</div>
+            </div>
 
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-              Create WOW! Deliver WOO!
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
           </div>
         </div>
         <div
